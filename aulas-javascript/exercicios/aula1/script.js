93e5dc68f9f3ae72ef9cb5cd586543e58556bb8e
Michele Santolin <michelesantolin@gmail.com>
2020-10-03 11:02:04 -0300

    exercicio 1 java

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,11 @@
+function checarSePodeDirigir(idade) {
+    if (idade > 18){
+        alert('Já pode dirigir!');
+    } else {
+        alert('Ainda não pode dirigir!');
+      }
+}
+
+let idade_usuario = prompt('Qual é sua idade?');
+
+checarSePodeDirigir(idade_usuario);
